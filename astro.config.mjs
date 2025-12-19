c5682555d0e8e6f88a231c0681e35f074ffce8d9
AchrafSL <thenewAchraf2020@gmail.com>
2025-12-19 21:13:12 +0100

    performance_update

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+  site: 'https://achrafsalimi.me', // Update this to your actual domain later!
   integrations: [mdx(), sitemap()],
 
   vite: {
